8a859c90ae91ad0e1edcc567e6799d97a6d1e0ec
Argenis Osorio <aosorio@cenditel.gob.ve>
2016-10-19 14:14:35 -0400

    Eliminados ficheros con sufijo .ex generados automáticamente

--- package_files/qpdfview-0.4.16/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(qpdfview):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="qpdfview" command="/usr/bin/qpdfview"
--- package_files/qpdfview-0.4.16/debian/qpdfview.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the qpdfview package
-#
-0 4	* * *	root	[ -x /usr/bin/qpdfview_maintenance ] && /usr/bin/qpdfview_maintenance
